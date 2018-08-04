d629cdc0ddbd8a08d3512cf3481d61e2c49714e3
ErikaStott <erika.r.stott@gmail.com>
2018-08-03 18:49:56 -0700

    Palindrome Component

--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -1,9 +1,47 @@
 import React, { Component } from 'react';
 
 export default class FilterString extends Component {
+    constructor(){
+        super();
+
+        this.state= {
+            unFilteredArray: [ "Erika", "Blake", "Christopher", "Keith" ],
+            userInput: '',
+            filteredArray: []
+        };
+    }
+    handleChange(val){
+        this.setState({userInput: val});
+    }
+    createsNewArray(userInput){
+            // const miniArrays= [];
+        var unFilteredArray= this.state.unFilteredArray;
+        const filteredArray= [];
+        
+        for(var i=0; i < unFilteredArray.length; i++){
+            if(unFilteredArray[i].includes(userInput)){
+                filteredArray.push(unFilteredArray[i]);
+                }
+            }
+            this.setState({filteredArray: filteredArray});
+        }
+    
+
+
     render(){
         return(
-            <p>FilterString Component</p>
+           <div className="puzzleBox filterStringPB">
+               <h4>Filter String</h4>
+               <span className="puzzleText">Unfiltered: {JSON.stringify(this.state.unFilteredArray, null, 10)}</span>
+               <input className="inputLine" onChange={ (e) => this.handleChange(e.target.value) }></input>
+               <button className="confirmationButton" onClick={ () => this.createsNewArray(this.state.userInput)}>Filter</button>
+               <span className="resultsBox filterStringRB"> Filtered:{JSON.stringify(this.state.filteredArray, null, 10)}</span>
+            </div>
         )
     }
 }
+
+
+//Using array of strings... ['', '', '', ''] filter out strings that contain a given string. 
+//Display a new array with strings that DO contain the given string
+//split array into multiple arrays.... split smaller arrays ... if smaller array contains input string then keep array.
--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -1,9 +1,46 @@
 import React, { Component } from 'react';
 
 export default class Palindrome extends Component {
+    constructor(){
+    super();
+
+    this.state= {
+        userInput: "",
+        palindrome: "",
+        };
+    }
+
+    handleChange(val){
+        this.setState({ userInput: val})
+    };
+
+    checkIfPalindrome(userInput){
+        var palindrome= this.state.palindrome;
+        const splitInput= input.split("");
+        if(splitInput === splitInput.reverse()){
+            palindrome= "true";
+        }
+        else{
+            palindrome= "false";
+        }
+           this.setState{
+            palindrome= palindrome;
+        };
+    }
+
+
+
     render(){
         return(
-            <p>Palindrome Component</p>
+            <div className="puzzleBox filterStringPB"> 
+                <h4>Palindrome</h4>
+                <input className="inputLine" onChange= {(e) => this.handleChange(e.target.value)}></input>
+                <button className="confirmationButton" onClick= {() => this.checkIfPalindrome(this.state.userInput)} > Check </button>
+                <span className="resultsBox">Palindrome: {this.state.palindrome}</span>
+            </div>
         )
     }
 }
+
+
+//Is the string spelled the same backwards and forwards?  loop forward and do nested loop backwards... compare each element
